d532a60408677c6bc0ebf82f61d3d52536174add
mikemajara <fmiguel05@gmail.com>
2020-05-10 13:32:21 +0200

    removed unused from main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from workflow.background import run_in_background, is_running
 
 DISPLAY_DETAILS = os.getenv('MM_DISPLAY_DETAILS').isdigit() and int(os.getenv('MM_DISPLAY_DETAILS'))
 DISPLAY_THUMBNAILS = os.getenv('MM_DISPLAY_THUMBNAILS').isdigit() and int(os.getenv('MM_DISPLAY_THUMBNAILS'))
-REFRESH_RATE = 0.5
+REFRESH_RATE = 0.2
 
 if DISPLAY_DETAILS or DISPLAY_THUMBNAILS:
     from pyquery import PyQuery as pq
@@ -28,34 +28,14 @@ def get_filmaffinity_suggestions(word):
     return web.post(url)
 
 
-def get_raw_html_for_url(url):
-    res = web.get(url)
-    return res.text if res.status_code == 200 else None
-
-
 def get_url_for_film_id(id):
     return "https://www.filmaffinity.com/es/film" + str(id) + ".html"
 
 
-def get_film_detail_string(id):
-    try:
-        html_raw = get_raw_html_for_url(get_url_for_film_id(id))
-        if html_raw is None:
-            return None
-
-        d = pq(html_raw)        
-        rating_node = d('div#movie-rat-avg')
-
-        if rating_node is not None:
-            rating_str = (rating_node.attr('content') or "-") + "/10 "
-        
-        return rating_str
-    except e:
-        log.debug("Unexpected exception")
-        return ""
-
 def is_result_type_movie(result):
     return re.search(r"movie-card-ac", result['label']) is not None
+
+
 
 
 def main(wf):    
